48cde21af307ca94257c92b360d3e9ed18b7223e
hidden <datproad126>
2019-10-06 13:08:12 +0000

    deleted datngodoan_175A071484

--- datngodoan_175A071484/bai_tap_thuc_hanh/JS.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-   redirectTime = "2000";
-   redirectURL = "register.html";
-   setTimeout("location.href = redirectURL;", redirectTime);
-   function reload() {
-      
-   }
-});
