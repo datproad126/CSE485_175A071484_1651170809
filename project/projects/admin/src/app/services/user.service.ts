d1aff87d58565d4bc7f1cbeed5ac210e1d8c4cd3
datproad126 <datngodoan123@gmail.com>
2019-10-09 06:45:35 +0700

    handle Remove button

--- project/projects/admin/src/app/services/user.service.ts
+++ project/projects/admin/src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { User } from '../model/user';
 import { Observable, throwError } from 'rxjs';
+import { catchError, retry, tap, map } from 'rxjs/operators';
 
 // declare http header
 const httpOptions = {
@@ -15,14 +16,25 @@ const httpOptions = {
 
 export class UserService {
   // declare api server
-  private PHP_API_SERVER = 'http://192.168.1.45:8080';
+  private PHP_API_SERVER = 'http://18.39.105.26:8080/api/user';
 
   constructor(private http: HttpClient) { }
   // get all user by httpclient
   readUserData(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.PHP_API_SERVER}/api/user/read.php`);
+    return this.http.get<any>(`${this.PHP_API_SERVER}/read.php`, httpOptions)
+      .pipe(
+        retry(3),
+        catchError(this.handleError),
+      );
   }
-  // error handle here 
+  // delete user
+  deleteData(user: User[]): Observable<HttpResponse<any>> {
+    return this.http.post<any>(`${this.PHP_API_SERVER}/delete.php`, user, { observe: 'response' })
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+  // error handle here
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
